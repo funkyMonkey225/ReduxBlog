57c65c40682078e0750954fbfe226eb271a7105c
Mac User <sabbey37@gmail.com>
2017-09-11 16:35:39 -0400

    Adds form fields

--- src/components/postsnew.js
+++ src/components/postsnew.js
@@ -2,9 +2,33 @@ import React, {Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input
+                    className="form-control" 
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+
     render() {
         return(
-            <div>Posts New!</div>
+            <form>
+                <Field 
+                    label="Title"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field 
+                    label="Tags"
+                    name="tags"
+                    component={this.renderField}
+                />
+            </form>
         );
     }
 
