2e0391a31ecb0c812341c1bee0c4f52c49d9e126
Mac User <sabbey37@gmail.com>
2017-09-11 18:50:11 -0400

    Deletes post from state once Delete button is clicked

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,11 @@
 import _ from 'lodash';
-import {FETCH_POSTS, FETCH_POST} from '../actions';
+import {FETCH_POSTS, FETCH_POST, DELETE_POST} from '../actions';
 
 export default function(state = {}, action) {
     switch(action.type) {
+        case DELETE_POST:
+            // If state object has key that matches post key payload, omit it and return a new object that does not include that id
+            return _.omit(state, action.payload);
         case FETCH_POST:
             // const post = action.payload.data;
             // const newState = {...state};
